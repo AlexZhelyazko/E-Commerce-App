b0ba0535965bd9678030520059fabc1c27edaf67
Alex <alex.zhelyazko@yandex.by>
2022-12-26 18:01:59 +0400

    add: arrows for slider

---
+++ src/components/Slider.jsx
@@ -0,0 +1,21 @@
+import { ArrowLeftOutlined, ArrowRightOutlined } from '@material-ui/icons';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+`;
+
+export const Slider = () => {
+  return (
+    <Container>
+      <Arrow>
+        <ArrowLeftOutlined />
+      </Arrow>
+      <Arrow>
+        <ArrowRightOutlined />
+      </Arrow>
+    </Container>
+  );
+};
