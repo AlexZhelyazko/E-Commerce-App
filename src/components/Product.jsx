a57a0c571b201ef8e8ff2adbe7a83b42a91bbe60
Alex <alex.zhelyazko@yandex.by>
2022-12-27 20:13:04 +0400

    add: animations

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -2,6 +2,22 @@ import { FavoriteBorderOutlined, SearchOutlined, ShoppingCartOutlined } from '@m
 import React from 'react';
 import styled from 'styled-components';
 
+const Info = styled.div`
+  opacity: 0;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.2);
+  z-index: 3;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: all 0.5s ease;
+  cursor: pointer;
+`;
+
 const Container = styled.div`
   flex: 1;
   margin: 5px;
@@ -12,6 +28,10 @@ const Container = styled.div`
   justify-content: center;
   background-color: #f5fbfd;
   position: relative;
+
+  &:hover ${Info} {
+    opacity: 1;
+  }
 `;
 
 const Circle = styled.div`
@@ -27,19 +47,6 @@ const Image = styled.img`
   z-index: 2;
 `;
 
-const Info = styled.div`
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  top: 0;
-  left: 0;
-  background-color: gray;
-  z-index: 3;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-
 const Icon = styled.div`
   width: 40px;
   height: 40px;
@@ -49,6 +56,12 @@ const Icon = styled.div`
   align-items: center;
   justify-content: center;
   margin: 10px;
+  transition: all 0.5s ease;
+
+  &:hover {
+    background-color: #e9f5f5;
+    transform: scale(1.1);
+  }
 `;
 
 export const Product = ({ item }) => {
