586e2409e86030a526e64224dec6134b1301edc1
Alex <alex.zhelyazko@yandex.by>
2022-12-29 14:41:09 +0400

    finish footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,4 @@
-import { Facebook, Instagram, Pinterest, Twitter } from '@material-ui/icons';
+import { Email, Facebook, Instagram, Phone, PinDrop, Pinterest, Twitter } from '@material-ui/icons';
 import styled from 'styled-components';
 
 const Container = styled.div`
@@ -13,10 +13,29 @@ const Left = styled.div`
 
 const Right = styled.div`
   flex: 1;
+  padding: 20px;
 `;
 
 const Center = styled.div`
+  padding: 20px;
   flex: 1;
+`;
+
+const Title = styled.h3`
+  margin-bottom: 30px;
+`;
+
+const List = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const ListItem = styled.li`
+  width: 50%;
+  margin-bottom: 10px;
 `;
 
 const Logo = styled.h1``;
@@ -25,9 +44,31 @@ const Description = styled.p`
   margin: 20px 0px;
 `;
 
-const SocialContainer = styled.div``;
+const SocialContainer = styled.div`
+  display: flex;
+`;
 
-const SocialIcon = styled.div``;
+const SocialIcon = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  color: white;
+  background-color: #${(props) => props.color};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 20px;
+`;
+
+const ContactItem = styled.div`
+  margin-bottom: 20px;
+  display: flex;
+  align-items: center;
+`;
+
+const Payment = styled.img`
+  width: 50%;
+`;
 
 export const Footer = () => {
   return (
@@ -40,22 +81,47 @@ export const Footer = () => {
           even slightly believable.
         </Description>
         <SocialContainer>
-          <SocialIcon>
+          <SocialIcon color="3B5999">
             <Facebook />
           </SocialIcon>
-          <SocialIcon>
+          <SocialIcon color="E4405F">
             <Instagram />
           </SocialIcon>
-          <SocialIcon>
+          <SocialIcon color="55ACEE">
             <Twitter />
           </SocialIcon>
-          <SocialIcon>
+          <SocialIcon color="E60023">
             <Pinterest />
           </SocialIcon>
         </SocialContainer>
       </Left>
-      <Center></Center>
-      <Right></Right>
+      <Center>
+        <Title>Useful Links</Title>
+        <List>
+          <ListItem>Home</ListItem>
+          <ListItem>Cart</ListItem>
+          <ListItem>Man Fashion</ListItem>
+          <ListItem>Woman Fashion</ListItem>
+          <ListItem>Accessories</ListItem>
+          <ListItem>My Account</ListItem>
+          <ListItem>Order Tracking</ListItem>
+          <ListItem>WishList</ListItem>
+          <ListItem>Terms</ListItem>
+        </List>
+      </Center>
+      <Right>
+        <Title>Contact</Title>
+        <ContactItem>
+          <PinDrop style={{ marginRight: '10px' }} /> 622 Dixie Path , South TobinChester 983336
+        </ContactItem>
+        <ContactItem>
+          <Phone style={{ marginRight: '10px' }} /> +74 123 43 12
+        </ContactItem>
+        <ContactItem>
+          <Email style={{ marginRight: '10px' }} /> app@gmail.com
+        </ContactItem>
+        <Payment src="https://i.ibb.co/Qfvn4z6/payment.png" />
+      </Right>
     </Container>
   );
 };
