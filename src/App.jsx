72194402755a29bf773a99e30cfddd5af722ffaf
Alex <alex.zhelyazko@yandex.by>
2023-01-05 16:04:47 +0400

    add: route

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,19 @@ import { ProductList } from './pages/ProductList';
 import { Register } from './pages/Register';
 import { Login } from './pages/Login';
 import { Cart } from './pages/Cart';
+import { Navigate, Route, Routes } from 'react-router';
 
 const App = () => {
+  let user = true;
   return (
-    <div>
-      <Home />
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/products/:category" element={<ProductList />} />
+      <Route path="/product/:id" element={<Product />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/login" element={user ? <Navigate replace to={'/'} /> : <Login />} />
+      <Route path="/register" element={user ? <Navigate replace to={'/'} /> : <Register />} />
+    </Routes>
   );
 };
 
